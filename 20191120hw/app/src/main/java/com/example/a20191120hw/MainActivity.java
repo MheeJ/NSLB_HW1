ab2f0948cc694991a056b6f05ef7cb0026ebd425
Minhee <unimini27@naver.com>
2019-12-04 22:32:44 +0900

    버튼 선언 축소

--- 20191120hw/app/src/main/java/com/example/a20191120hw/MainActivity.java
+++ 20191120hw/app/src/main/java/com/example/a20191120hw/MainActivity.java
@@ -18,12 +18,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Manager manager = new Manager();
     private General general = new General();
     private Vip vip = new Vip();
-    private Button mBtnLog, mBtnManage, mBtnWrite, mBtnRemove, mBtnRead;
+   /* private Button mBtnLog, mBtnManage, mBtnWrite, mBtnRemove, mBtnRead;*/
+    private Button [] mBtnArray = new Button[5];
+/*
     private String a = "a";
     private String b = "a";
     private String c = "a";
     private String d = "a";
-
+*/
+    private String action[] = new String[4];
+    private String act_msg[] = {"글쓰기","삭제","읽기","관리"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,34 +39,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void initGUI(){
+    private void initGUI() {
 
-        mLogin = (EditText)findViewById(R.id.text_log);
-        mBtnManage = (Button)findViewById(R.id.btn_manage);
+        for (int i = 0; i < action.length; i++) {
+            action[i] = "a";
+        }
+
+        for (int j = 0; j < 5; j++) {
+            mBtnArray[j] = (Button) findViewById(R.id.btn1 + j);
+            mBtnArray[j].setOnClickListener(this);
+
+        }
+        mLogin = (EditText) findViewById(R.id.text_log);
+       /* mBtnManage = (Button)findViewById(R.id.btn_manage);
         mBtnLog = (Button)findViewById(R.id.btn_log);
         mBtnRead = (Button)findViewById(R.id.btn_read);
         mBtnRemove = (Button)findViewById(R.id.btn_remove);
-        mBtnWrite = (Button)findViewById(R.id.btn_write);
-        mManage = (TextView)findViewById(R.id.text_power);
+        mBtnWrite = (Button)findViewById(R.id.btn_write);*/
+        mManage = (TextView) findViewById(R.id.text_power);
 
         //this 뭐 적는거 있음
         manager.onMemberListener(this);
         general.onMemberListener(this);
         vip.onMemberListener(this);
 
-        mBtnWrite.setOnClickListener(this);
+      /*  mBtnWrite.setOnClickListener(this);
         mBtnRemove.setOnClickListener(this);
         mBtnManage.setOnClickListener(this);
         mBtnRead.setOnClickListener(this);
-        mBtnLog.setOnClickListener(this);
-    }
+        mBtnLog.setOnClickListener(this);*/
 
+    }
 
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_log:
+
+            case R.id.btn5:
                 text_login = mLogin.getText().toString();
                 if(text_login.equals("일반회원")){
                     mManage.setText(text_login);
@@ -82,8 +96,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
 
-            case R.id.btn_write:
-                   if(a.equals("YES")) {
+
+
+            case R.id.btn1:
+                   if(action[0].equals("YES")) {
                        mManage.setText("글쓰기");
                    }
                    else {
@@ -91,8 +107,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                    }
                    break;
 
-            case R.id.btn_remove :
-                   if(b.equals("YES")) {
+            case R.id.btn2:
+                   if(action[1].equals("YES")) {
                        mManage.setText("삭제");
                    }
                    else {
@@ -100,8 +116,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                    }
                    break;
 
-            case R.id.btn_read :
-                   if(c.equals("YES")) {
+            case R.id.btn3 :
+                   if(action[2].equals("YES")) {
                        mManage.setText("읽기");
                    }
                    else {
@@ -109,8 +125,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                    }
                    break;
 
-            case R.id.btn_manage :
-                   if(d.equals("YES")) {
+            case R.id.btn4 :
+                   if(action[3].equals("YES")) {
                        mManage.setText("관리");
                    }
                    else {
@@ -124,12 +140,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
        }
 
 
-
     public void onDo(String write, String remove, String read, String mag){
-        a = write;
-        b = remove;
-        c = read;
-        d = mag;
+        action[0] = write;
+        action[1] = remove;
+        action[2] = read;
+        action[3] = mag;
     }
 
 
